266c5f8066cb17624921553a3981380b6c418725
chinmay-kothari <chinmay3199@gmail.com>
2020-10-02 02:26:19 +0530

    Project Upload

---
+++ Assets/Script/Manager/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Script.Manager
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance = GameObject.FindGameObjectWithTag("GameController").AddComponent<T>();
+
+        public static T Instance
+        {
+            get { return instance; }
+        }
+
+        private void OnApplicationQuit()
+        {
+            instance = null;
+        }
+    }
+}
